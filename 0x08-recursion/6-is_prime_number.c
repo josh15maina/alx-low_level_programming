aeb96d333d481911143676e2f77680a6c0677a34
Joshua Maina <josh15maina@gmail.com>
2022-03-31 16:52:09 +0000

    Hitch Slap

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+int isprime(int c, int d);
+
+/**
+ * isprime -  Makes possible to evaluate from 1 to n
+ * @c: same number as n
+ * @d: number that iterates from 1 to n
+ *
+ * Return: Prime number
+ */
+int isprime(int c, int d)
+{
+	if (c == d)
+	{
+		return (1);
+	}
+	else if (c % d == 0)
+	{
+		return (0);
+	}
+	return (isprime(c, d + 1));
+}
+
+/**
+ * is_prime_number - checks if a number is prime
+ * @n: Number Integer
+ *
+ * Return: Prime number
+ */
+int is_prime_number(int n)
+{
+	if (n <= 0)
+		return (0);
+	return (isprime(n, 2));
+}

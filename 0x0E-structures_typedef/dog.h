46d72a02bc93938bed7f53e6a6a7ea2d1a19b5cd
Joshua Maina <josh15maina@gmail.com>
2022-04-12 19:48:19 +0000

    Modus Vivendi

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - description for a pet
+ * @name: pet name
+ * @age: pet age
+ * @owner: owner
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};

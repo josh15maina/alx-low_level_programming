cbe24ad6de011058f0c6ff487e4b9bc1e4b40dc4
Joshua Maina <josh15maina@gmail.com>
2022-04-28 22:50:25 +0000

    Hush

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * set_bit - sets the value of a bit to 1 at a given index
+ * clear_bit - sets the value of a bit to 0 at a given index
  * @n: base 10 number
  * @index: index to set to 1
  *
  * Return: 1 if it worked, or -1 if an error occurred
  *
  */
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int mask;
 

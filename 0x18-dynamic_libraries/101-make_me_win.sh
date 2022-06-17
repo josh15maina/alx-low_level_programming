0dc14096902a2b765ed8f9cb6127eb7931f07a0e
Joshua Maina <josh15maina@gmail.com>
2022-06-17 14:58:03 +0000

    Southern

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/holbertonschool/0x18.c/blob/master/101-md5_gm
-export LD_PRELOAD=/tmp/putshack.so
+wget -P /tmp/ https://raw.github.com/josh15maina/alx-low_level_programming/master/0x18-dynamic_libraries/jackpot.so
+export LD_PRELOAD=/tmp/jackpot.so

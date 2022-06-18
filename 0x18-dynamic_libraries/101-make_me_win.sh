7b7851aae6c5efbc0863a476d81098d0d36da93d
Joshua Maina <josh15maina@gmail.com>
2022-06-18 01:29:53 +0000

    Stress

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/josh15maina/alx-low_level_programming/blob/master/0x17-doubly_linked_lists/liball.so
-export LD_PRELOAD=/tmp/liball.so
+wget -P /tmp https://github.com/josh15maina/alx-low_level_programming/raw/master/0x18-dynamic_libraries/jackpot.so
+export LD_PRELOAD=/tmp/jackpot.so
